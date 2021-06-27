7c7a4d47f8075ffae39fddf4a776940bba259b28
Paul Anthony McGowan <xhostcom@gmail.com>
2021-06-27 11:14:54 +0100

    Updates

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -25,7 +25,7 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to @tweet, notice: "Tweet was successfully created." }
+        format.html { redirect_to @tweet, notice: "Twonk was successfully created." }
         format.json { render :show, status: :created, location: @tweet }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class TweetsController < ApplicationController
   def update
     respond_to do |format|
       if @tweet.update(tweet_params)
-        format.html { redirect_to root_path, notice: "Tweet was successfully updated." }
+        format.html { redirect_to root_path, notice: "Twonk was successfully updated." }
         format.json { render :show, status: :ok, location: @tweet }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -51,7 +51,7 @@ class TweetsController < ApplicationController
   def destroy
     @tweet.destroy
     respond_to do |format|
-      format.html { redirect_to tweets_url, notice: "Tweet was successfully destroyed." }
+      format.html { redirect_to tweets_url, notice: "Twonk was successfully destroyed." }
       format.json { head :no_content }
     end
   end
